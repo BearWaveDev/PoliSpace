0ab6259559730d69ac6785fdf197e8409ca09ec4
Pancito00 <ashleymedinamontesdeoca@gmail.com>
2026-09-19 22:04:08 -0600

    Integracion de boton de iniciar sesion y logica Java para la accion de este, se puede mofificar en un futuro

--- PoliSpace App/app/src/main/java/com/example/polispace/MainActivity.kt
+++ PoliSpace App/app/src/main/java/com/example/polispace/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.polispace
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val btnLogin = findViewById<MaterialButton>(R.id.btnLogin)
+
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, Iniciarsesion::class.java)
+            startActivity(intent)
+        }
+
+        // botón de registrarse:
+        /*
+        val btnRegister = findViewById<MaterialButton>(R.id.btnRegister)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        */
     }
 }
